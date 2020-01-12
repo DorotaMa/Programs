64c9bcfe0c004e08a45cde94300183ce14ef7ce3
DorotaMa <malinowska.dorota@vp.pl>
2020-01-12 10:14:55 +0100

    Homework

---
+++ 7_NWD_Euklides.py
@@ -0,0 +1,18 @@
+""" Zadanie 7 Stwórz funkcję do znajdowania największego wspólnego dzielnika dwóch
+liczb. (algorytm Euklidesa) """
+
+def Euklides(a,b):
+    # funkcja znajduje najwiekszy wspolny dzielnik dwoch liczb
+    lst = [a, b]
+    a = max(lst)
+    b = min(lst)
+    c = a % b
+    print(a,b,c)
+    if c != 0:
+        a = b
+        b = c
+        return Euklides(a, b)
+    else:
+        return print(f"{b} jest najwiekszym wspolnym dzielnikiem liczb a i b")
+
+Euklides(282,78)
